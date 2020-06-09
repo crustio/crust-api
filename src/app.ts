943d99bd0a9cfa9cfb3fdb0c8049371d7b52c257
yuhui <1161569428@qq.com>
2020-06-09 14:24:09 +0800

    upgrade polkadotjs && log with request time

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import * as bodyParser from 'body-parser';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { Keyring } from '@polkadot/api';
 import { KeyringPair } from '@polkadot/keyring/types';
+const moment = require('moment');
 
 // Creates and configures an ExpressJS web server.
 class App {
@@ -22,59 +23,59 @@ class App {
             types: {
                 Address: "AccountId",
                 Identity: {
-                    "pub_key": "Vec<u8>",
-                    "account_id": "AccountId",
-                    "validator_pub_key": "Vec<u8>",
-                    "validator_account_id": "AccountId",
-                    "sig": "Vec<u8>"
+                    pub_key: 'Vec<u8>',
+                    account_id: 'AccountId',
+                    validator_pub_key: 'Vec<u8>',
+                    validator_account_id: 'AccountId',
+                    sig: 'Vec<u8>'
                 },
                 WorkReport: {
-                    pub_key: "Vec<u8>",
-                    block_number: "u64",
-                    block_hash: "Vec<u8>",
-                    used: "u64",
-                    reserved: "u64",
-                    files: "Vec<(Vec<u8>, u64)>",
-                    sig: "Vec<u8>"
+                    pub_key: 'Vec<u8>',
+                    block_number: 'u64',
+                    block_hash: 'Vec<u8>',
+                    used: 'u64',
+                    reserved: 'u64',
+                    files: 'Vec<(Vec<u8>, u64)>',
+                    sig: 'Vec<u8>'
                 },
                 StakingLedger: {
-                    stash: "AccountId",
-                    total: "Compact<Balance>",
-                    active: "Compact<Balance>",
-                    valid: "Compact<Balance>",
-                    unlocking: "Vec<UnlockChunk>",
+                    stash: 'AccountId',
+                    active: 'Compact<Balance>',
+                    total: 'Compact<Balance>',
+                    valid: 'Compact<Balance>',
+                    unlocking: 'Vec<UnlockChunk>',
                 },
                 Validations: {
-                    total: "Compact<Balance>",
-                    guarantee_fee: "Compact<Perbill>",
-                    guarantors: "Vec<AccountId>",
+                    total: 'Compact<Balance>',
+                    guarantee_fee: 'Compact<Perbill>',
+                    guarantors: 'Vec<AccountId>',
                 },
                 Nominations: {
-                    targets: "Vec<AccountId>",
-                    total: "Compact<Balance>",
-                    submitted_in: "u32",
-                    suppressed: "bool"
+                    targets: 'Vec<AccountId>',
+                    total: 'Compact<Balance>',
+                    submitted_in: 'u32',
+                    suppressed: 'bool'
                 },
-                ReportSlot: "u64",
-                Weight: "u32",
-                LookupSource: "AccountId",
-                AddressInfo: "Vec<u8>",
-                MerkleRoot: "Vec<u8>",
+                ReportSlot: 'u64',
+                AddressInfo: 'Vec<u8>',
+                MerkleRoot: 'Vec<u8>',
                 Provision: {
-                    address: "Vec<u8>",
-                    file_map: "Vec<(Vec<u8>, Hash)>"
+                    address: 'Vec<u8>',
+                    file_map: 'Vec<(Vec<u8>, Vec<Hash>)>'
                 },
                 OrderStatus: {
-                    '_enum': ['Success', 'Failed', 'Pending']
+                    _enum: ['Success', 'Failed', 'Pending']
                 },
                 StorageOrder: {
-                    file_identifier: "Vec<u8>",
-                    file_size: "u64",
-                    created_on: "BlockNumber",
-                    expired_on: "BlockNumber",
-                    provider: "AccountId",
-                    client: "AccountId",
-                    order_status: "OrderStatus"
+                    file_identifier: 'Vec<u8>',
+                    file_size: 'u64',
+                    created_on: 'BlockNumber',
+                    completed_on: 'BlockNumber',
+                    expired_on: 'BlockNumber',
+                    provider: 'AccountId',
+                    client: 'AccountId',
+                    amount: 'Balance',
+                    order_status: 'OrderStatus'
                 }
             }
         });
@@ -94,7 +95,7 @@ class App {
         if (len % 2 != 0) {
             return null;
         }
-    
+
         len /= 2;
         let hexA = new Array();
         for (let i = 0; i < len; i++) {
@@ -103,7 +104,7 @@ class App {
             hexA.push(v);
             pos += 2;
         }
-    
+
         return hexA;
     }
 
@@ -116,10 +117,10 @@ class App {
 
         let result = "";
         for (let i = 0; i < bytes.length; i++) {
-          result += String.fromCharCode(bytes[i]);
+            result += String.fromCharCode(bytes[i]);
         }
         return result;
-      }
+    }
 
     // Generate user from backup and password
     private generateUser(backup: string, password: string) {
@@ -133,12 +134,14 @@ class App {
         let router = express.Router();
 
         router.get('/', (req, res, next) => {
+            console.log('request time: ', moment().format())
             res.json({
                 message: 'This is crust api.'
             });
         });
 
         router.get('/api/v1/block/header', (req, res, next) => {
+            console.log('request time: ', moment().format())
             this.api.then(async (api) => {
                 const lastHeader = await api.rpc.chain.getHeader();
                 res.json({
@@ -152,6 +155,7 @@ class App {
         });
 
         router.get('/api/v1/block/hash', (req, res, next) => {
+            console.log('request time: ', moment().format())
             // Get address
             const blockNumber = req.query["blockNumber"];
             if (typeof blockNumber !== "string") {
@@ -170,6 +174,7 @@ class App {
         });
 
         router.get('/api/v1/tee/identity', (req, res, next) => {
+            console.log('request time: ', moment().format())
             // Get address
             const address = req.query["address"];
             if (typeof address !== "string") {
@@ -188,6 +193,7 @@ class App {
         });
 
         router.get('/api/v1/tee/workreport', (req, res, next) => {
+            console.log('request time: ', moment().format())
             // Get address
             const address = req.query["address"];
             if (typeof address !== "string") {
@@ -206,6 +212,7 @@ class App {
         });
 
         router.get('/api/v1/market/provider', (req, res, next) => {
+            console.log('request time: ', moment().format())
             // 1. Get address
             const address = req.query["address"];
             if (typeof address !== "string") {
@@ -226,6 +233,7 @@ class App {
         });
 
         router.get('/api/v1/market/sorder', (req, res, next) => {
+            console.log('request time: ', moment().format())
             // 1. Get order id
             const orderId = req.query["orderId"];
             if (typeof orderId !== "string") {
@@ -244,6 +252,7 @@ class App {
         });
 
         router.post('/api/v1/tee/identity', (req, res, next) => {
+            console.log('request time: ', moment().format())
             //Get identity
             const identity = req.body['identity'];
             if (typeof identity !== "string") {
@@ -261,7 +270,7 @@ class App {
                 sig: "0x" + identityjson["sig"]
             }
 
-            console.log(identityInstance)
+            console.log('request param, time', identityInstance, new Date())
 
             //Get backup
             const backup = req.body["backup"];
@@ -310,13 +319,14 @@ class App {
         });
 
         router.post('/api/v1/tee/workreport', (req, res, next) => {
+            console.log('request time: ', moment().format())
             //Get workreport
             const workReport = req.body['workreport'];
             if (typeof workReport !== "string") {
                 res.status(400).send('Please add workreport (type is string) to the request body.');
                 return;
             }
-            
+
             // Construct work report
             const workReportJson = JSON.parse(workReport.toString());
 
@@ -382,6 +392,7 @@ class App {
         });
 
         router.post('/api/v1/market/register', (req, res, next) => {
+            console.log('request time: ', moment().format())
             // 1. Get and check address info
             const addressInfo = req.body['addressInfo'];
             if (typeof addressInfo !== "string") {
@@ -424,11 +435,11 @@ class App {
                             msg: "register success",
                         });
                     }
-                    if (status.isFinalityTimeout || 
-                        status.isInvalid || 
+                    if (status.isFinalityTimeout ||
+                        status.isInvalid ||
                         status.isDropped) {
-                            res.status(400).send('Invalid extrinsic');
-                        }
+                        res.status(400).send('Invalid extrinsic');
+                    }
                 }).catch(e => {
                     res.status(500).send(`${e}`);
                     return;
@@ -440,6 +451,7 @@ class App {
         })
 
         router.post('/api/v1/market/sorder', (req, res, next) => {
+            console.log('request time: ', moment().format())
             // 1. Get and check storage order
             let sorder = req.body['sorder'];
             if (typeof sorder !== "string") {
@@ -488,7 +500,7 @@ class App {
                         api.query.market.providers(sorder.provider).then(async (provider) => {
                             const providerJson = JSON.parse(provider.toString());
                             let orderId = "";
-                            for( const fm of providerJson.file_map) {
+                            for (const fm of providerJson.file_map) {
                                 console.log(fm);
                                 if (fm[0] == sorder.fileIdentifier) {
                                     orderId = fm[1];
@@ -504,11 +516,11 @@ class App {
                             }
                         });
                     }
-                    if (status.isFinalityTimeout || 
-                        status.isInvalid || 
+                    if (status.isFinalityTimeout ||
+                        status.isInvalid ||
                         status.isDropped) {
-                            res.status(400).send('Invalid extrinsic');
-                        }
+                        res.status(400).send('Invalid extrinsic');
+                    }
                 }).catch(e => {
                     res.status(500).send(`${e}`);
                     return;
