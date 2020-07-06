a670a569dfe5256a4af55cbcc58d07cc6afdb31f
yuhui <1161569428@qq.com>
2020-07-06 11:07:45 +0800

    fix build

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import * as express from 'express';
+import express from 'express';
 import * as bodyParser from 'body-parser';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { Keyring } from '@polkadot/api';
